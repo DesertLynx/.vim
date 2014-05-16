795da0459fe10a52b7039b0926656251fdcc4cba
DesertLynx <contact@davidporter.id.au>
2014-05-16 18:22:14 +1000

    Updated installer

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install make curl vim git dtrx zsh wget more-utils
-sudo curl -L http://install.ohmyz.sh | sh
+
+#install Zsh
+if [ ! -d "~/.oh-my-zsh" ]; then
+	sudo curl -L http://install.ohmyz.sh | sh
+fi
+
+#install vim 
 if [ ! -d "~/.vim" ]; then
 	cd ~
 	git clone https://github.com/DesertLynx/.vim 
